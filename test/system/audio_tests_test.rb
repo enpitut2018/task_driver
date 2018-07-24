5f6cb460a034a9ebca6c4230c35b3e2ca609b7be
Kunado <katsusand1@gmail.com>
2018-07-24 15:31:39 +0900

    add task pages with sounds

---
+++ test/system/audio_tests_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class AudioTestsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit audio_tests_url
+  #
+  #   assert_selector "h1", text: "AudioTest"
+  # end
+end
