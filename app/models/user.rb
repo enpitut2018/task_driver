68fe490975d22171d15f83afc5be9493ade6b539
Yuki Oba <s1611488@klis.tsukuba.ac.jp>
2018-07-25 13:16:01 +0900

    Add devise views

--- app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ApplicationRecord
-    mount_uploader :image, ImagesUploader
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
   get 'pages/page'
-  resources :users
+  
+  #ルート
+  root 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
