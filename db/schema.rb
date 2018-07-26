349be1af2b1f1ee16bf2de291dce873fd5e90242
chr <stchr820.6281.kkt@gmail.com>
2018-07-26 15:15:18 +0900

    ユーザ毎のタスク検索と表示ができるようになりました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180725041640) do
+ActiveRecord::Schema.define(version: 20180726013816) do
 
   create_table "audio_tests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180725041640) do
     t.datetime "finish_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
