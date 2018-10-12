a2e8a03d3f39033a0d6b8252ad97aecb76dfb666
Yuki Oba <s1611488@klis.tsukuba.ac.jp>
2018-10-12 16:03:56 +0900

    Add group_id column into tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180727044426) do
+ActiveRecord::Schema.define(version: 20181012070236) do
 
   create_table "audio_tests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180727044426) do
     t.integer "user_id"
     t.integer "urgency"
     t.integer "priority"
+    t.integer "group_id"
   end
 
   create_table "users", force: :cascade do |t|
