8befa4d9fb3bd1884c9f51996c2e88f756cd326b
Yuki Oba <s1611488@klis.tsukuba.ac.jp>
2018-10-19 16:30:03 +0900

    Change group and task pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   
   #ルート
   root 'tasks#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'  
+   }
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
