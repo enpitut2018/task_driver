a051d7bccc24464f089d337ec90144d7ab22f749
chr <stchr820.6281.kkt@gmail.com>
2018-11-14 15:03:27 +0900

    add migration for user auth

--- db/migrate/20181114_add_user_auth.rb
+++
@@ -1,8 +0,0 @@
-class AddUserEndpoint < ActiveRecord::Migration[5.1]
-    def change
-      add_column :users, :key, :string
-      add_column :users, :auth, :string
-      add_column :users, :encoding, :string
-    end
-  end
-  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181017051539) do
+ActiveRecord::Schema.define(version: 20181114060139) do
 
   create_table "audio_tests", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 20181017051539) do
     t.string "uid"
     t.string "username"
     t.string "endpoint"
+    t.string "key"
+    t.string "auth"
+    t.string "encoding"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
