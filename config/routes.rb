d3d493afcb1d738617967168ea774b8be7a02b6b
kunado <katsusand1@gmail.com>
2018-11-25 07:53:37 +0900

    settig for talking with frontend using token

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,46 @@
 Rails.application.routes.draw do
+
   post 'groups/fork'
-  # resources :groups
+  resources :groups
+
   get 'tasks/done'
-  # resources :tasks
-  get 'pages/page'
   post 'tasks/status_change'
+  resources :tasks
+
+  get 'pages/page'
+
   get 'endpoints', to: 'endpoints#register'
 
-  
   #ルート
   root 'tasks#index'
   
-  devise_for :users, controllers: { 
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'  
-   }
+  # devise_for :users, controllers: { 
+  #   omniauth_callbacks: 'users/omniauth_callbacks',
+  #   :registrations => 'users/registrations',
+  #   :sessions => 'users/sessions'  
+  #  }
 
-  devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy" 
-  end
+  # devise_scope :user do
+  #   get "sign_in", :to => "users/sessions#new"
+  #   get "sign_out", :to => "users/sessions#destroy" 
+  # end
 
   scope :v1, defaults: { format: :json } do
     resources :groups
     resources :tasks
+
+    devise_for :users, controllers: { 
+      omniauth_callbacks: 'users/omniauth_callbacks',
+      :registrations => 'users/registrations',
+      :sessions => 'users/sessions'
+    }
+
+    devise_scope :user do
+      get 'sign_in', :to => 'users/sessions#new'
+      get 'sign_out', :to => 'users/sessions#destroy'
+      get 'users/current', :to => 'users/sessions#show'
+    end
+
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
