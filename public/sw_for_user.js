b8640e9d3aec768505af533f3a01b4d5308c2bac
Ryoma-Suizu <s1611443@u.tsukuba.ac.jp>
2018-11-25 13:55:54 +0900

    pull

--- public/sw_for_user.js
+++ public/sw_for_user.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-self.addEventListener('push', function(evt){
-    console.log("Notification start");
-    evt.waitUntil(
-        self.registration.showNotification('今だらだらしてた？',
-            {
-                body: "今暇か？",
-                actions: [{
-                    action: "yes",
-                    title: "暇や"
-                }, {
-                    action: "no",
-                    title: "暇じゃない"
-                }],
-                vibrate: [200, 100, 200, 100, 200, 100, 200]
-            }
-        )
-    );
-}, false);
-
-self.addEventListener('notificationclick', function(event){
-    event.notification.close();
-        if(event.action == "yes"){
-            self.registration.showNotification(
-                'やっていく気持ち', 
-                {
-                    body: "タスクやる？",
-                    actions: [{
-                        action: "yes2",
-                        title: "やっていき"
-                    }, {
-                        action: "no2",
-                        title: "ダメ"
-                    }
-                    ]
-                }
-            )
-        }
-        else if(event.action == "yes2"){
-            console.log("openwindow");
-            clients.openWindow("/tasks/94?timer=1");
-        }
-=======
 self.addEventListener('push', function(evt){
     console.log("Notification start");
 
@@ -70,5 +27,4 @@ self.addEventListener('notificationclick', function(event){
     event.notification.close();
     console.log("openwindow");
     clients.openWindow(event.notification.data != null ? event.notification.data.target_url : '/');
->>>>>>> 679a1ae9f6ca593c00bd365604ad0fa6fbae16aa
 }, false);
