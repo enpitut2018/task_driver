7bb00a1df9bd90f0a88f080335df2604900f3c20
kunado <katsusand1@gmail.com>
2018-11-27 21:01:42 +0900

    add description of queries

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -1,17 +1,18 @@
 Types::TaskType = GraphQL::ObjectType.define do
   name "Task"
-  field :id, !types.ID
-  field :name, !types.String
-  field :deadline, !types.String
-  field :importance, !types.Int
-  field :note, types.String
-  field :status, !types.Int
-  field :start_time, types.String
-  field :finish_time, types.String
-  field :created_at, !Types::MomentType
-  field :updated_at, !Types::MomentType
-  field :user_id, !types.ID
-  field :urgency, types.Int
-  field :priority, types.Int
-  field :group_id, !types.ID
+
+  field :id, !types.ID, 'タスクID'
+  field :name, !types.String, 'タスク名'
+  field :deadline, !Types::MomentType, 'タスク締め切り'
+  field :importance, !types.Int, '重要度'
+  field :note, types.String, 'メモ'
+  field :status, !types.Int, '進行状態(0: TODO, 1: DOING, 2: DONE)'
+  field :start_time, Types::MomentType, '開始時刻'
+  field :finish_time, Types::MomentType, '終了時刻'
+  field :created_at, !Types::MomentType, '作成時刻'
+  field :updated_at, !Types::MomentType, '更新時刻'
+  field :user_id, !types.ID, 'ユーザーID'
+  field :urgency, types.Int, '緊急度'
+  field :priority, types.Int, '優先度'
+  field :group_id, !types.ID, 'グループID'
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,9 +1,9 @@
 Types::UserType = GraphQL::ObjectType.define do
   name "User"
-  field :id, !types.ID
-  field :email, !types.String
-  field :username, types.String
-  field :created_at, !Types::MomentType
-  field :updated_at, !Types::MomentType
-  connection :groups, !Types::GroupType.connection_type
+  field :id, !types.ID, 'ユーザーID'
+  field :email, !types.String, 'メールアドレス'
+  field :username, types.String, 'ユーザー名'
+  field :created_at, !Types::MomentType, '作成時刻'
+  field :updated_at, !Types::MomentType, '更新時刻'
+  connection :groups, !Types::GroupType.connection_type, '所有グループ'
 end
