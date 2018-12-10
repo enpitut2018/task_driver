564522a0fdb00e1cafdbb3289f446c07448bb494
kunado <katsusand1@gmail.com>
2018-12-10 14:43:41 +0900

    upgrade existing queries

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,13 +1,10 @@
-Types::MutationType = GraphQL::ObjectType.define do
-  name "Mutation"
-  
-  field :startTaskMutation, Mutations::StartTaskMutation.field
-  field :createCrapMutation, Mutations::CreateCrapMutation.field
+class Types::MutationType < Types::BaseObject
+  field :start_task_mutation, Mutations::StartTaskMutation.field, null: true
+  field :create_crap_mutation, Mutations::CreateCrapMutation.field, null: true
   # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+  field :test_field, String, description: "An example field added by the generator", null: true
+
+  def test_field
+    "Hello World!"
   end
 end
--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -1,21 +1,18 @@
-Types::TaskType = GraphQL::ObjectType.define do
-  name "Task"
-
-  field :id, !types.ID, 'タスクID'
-  field :name, !types.String, 'タスク名'
-  field :deadline, !Types::MomentType, 'タスク締め切り'
-  field :importance, !types.Int, '重要度'
-  field :note, types.String, 'メモ'
-  field :status, !types.Int, '進行状態(0: TODO, 1: DOING, 2: DONE)'
-  field :start_time, Types::MomentType, '開始時刻'
-  field :finish_time, Types::MomentType, '終了時刻'
-  field :created_at, !Types::MomentType, '作成時刻'
-  field :updated_at, !Types::MomentType, '更新時刻'
-  field :user_id, !types.ID, 'ユーザーID'
-  field :urgency, types.Int, '緊急度'
-  field :priority, types.Int, '優先度'
-  field :group_id, !types.ID, 'グループID'
-  field :crap, !types.Int, '拍手'
-  connection :contributions, !Types::ContributionType.connection_type, 'コントリビューション'
-  
+class Types::TaskType < Types::BaseObject
+  field :id, ID, 'タスクID', null: false
+  field :name, String, 'タスク名', null: false
+  field :deadline, Types::MomentType, 'タスク締め切り', null: false
+  field :importance, Integer, '重要度', null: false
+  field :note, String, 'メモ', null: true
+  field :status, Integer, '進行状態(0: TODO, 1: DOING, 2: DONE)', null: false
+  field :start_time, Types::MomentType, '開始時刻', null: true
+  field :finish_time, Types::MomentType, '終了時刻', null: true
+  field :created_at, Types::MomentType, '作成時刻', null: false
+  field :updated_at, Types::MomentType, '更新時刻', null: false
+  field :user_id, ID, 'ユーザーID', null: false
+  field :urgency, Integer, '緊急度', null: true
+  field :priority, Integer, '優先度', null: true
+  field :group_id, ID, 'グループID', null: false
+  field :crap, Integer, '拍手', null: false
+  field :contributions, Types::ContributionType.connection_type, 'コントリビューション', null: false, connection: true
 end
