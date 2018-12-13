11793739e81a14304d10098d4744867bbade2320
kunado <katsusand1@gmail.com>
2018-12-14 06:30:58 +0900

    set finality and status as enum and add uniqueness validation to set of user_id, task_id and status when creating new contribution record

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,9 @@
 class Contribution < ApplicationRecord
   belongs_to :user
   belongs_to :task
+
+  enum finality: { final: true, not_final: false }
+  enum status: { active: true, inactive: false }
+
+  validates :status, uniqueness: { scope: [:user_id, :task_id], on: :create }
 end
