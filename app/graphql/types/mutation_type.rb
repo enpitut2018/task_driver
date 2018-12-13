b7fe809da5e0fd9b3c803cec3d136ac4e8f751de
kunado <katsusand1@gmail.com>
2018-12-14 08:40:47 +0900

    rewrite finish contribution mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 class Types::MutationType < GraphQL::Schema::Object
-  field :startTask, mutation: Mutations::StartTask
+  field :startContribution, mutation: Mutations::StartContribution
+  field :finishContribution, mutation: Mutations::FinishContribution
   field :createClap, mutation: Mutations::CreateClap
   
   # TODO: Remove me
