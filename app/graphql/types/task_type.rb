f6ae57d2ed41e3bc12a56fa6b373f410e57e6095
kunado <katsusand1@gmail.com>
2018-12-14 17:01:23 +0900

    update task type

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -4,7 +4,7 @@ class Types::TaskType < Types::BaseObject
   field :deadline, Types::MomentType, 'タスク締め切り', null: false
   field :importance, Integer, '重要度', null: false
   field :note, String, 'メモ', null: true
-  field :status, Integer, '進行状態(0: TODO, 1: DOING, 2: DONE)', null: false
+  field :status, Integer, '進行状態(1: TODO, 2: DOING, 3: DONE)', null: false
   field :start_time, Types::MomentType, '開始時刻', null: true
   field :finish_time, Types::MomentType, '終了時刻', null: true
   field :created_at, Types::MomentType, '作成時刻', null: false
@@ -13,11 +13,15 @@ class Types::TaskType < Types::BaseObject
   field :urgency, Integer, '緊急度', null: true
   field :priority, Integer, '優先度', null: true
   field :group_id, ID, 'グループID', null: false
-  field :crap, Integer, '拍手', null: false
+  field :clap, Integer, '拍手', null: false
   field :contributions, [Types::ContributionType], null: true
   
   def contriubtions
     description 'コントリビューション'
     object.contributions
+  end
+
+  def status
+    Task.statuses[object.status]
   end
 end
