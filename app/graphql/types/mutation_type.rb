11a16ba469902af38d84b151fd8f0e7e7b06b83a
kunado <katsusand1@gmail.com>
2018-12-17 23:35:53 +0900

    add create and update mutations into mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,11 @@
 class Types::MutationType < GraphQL::Schema::Object
-  
+
   field :startContribution, mutation: Mutations::StartContribution
   field :finishContribution, mutation: Mutations::FinishContribution
-  field :createClap, mutation: Mutations::CreateClap
+
   field :createTask, mutation: Mutations::CreateTask
+  field :updateTask, mutation: Mutations::UpdateTask
+  field :deleteTask, mutation: Mutations::DeleteTask
+
+  field :createClap, mutation: Mutations::CreateClap
 end
