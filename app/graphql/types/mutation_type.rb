82249846d85ab80f041edeb29c4bf043b48f75f3
kunado <katsusand1@gmail.com>
2018-12-17 23:34:30 +0900

    delete unnecessary mutations and update mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,14 +1,7 @@
 class Types::MutationType < GraphQL::Schema::Object
-  field :createTask, mutation: Mutations::CreateTaskMutation
+  
   field :startContribution, mutation: Mutations::StartContribution
   field :finishContribution, mutation: Mutations::FinishContribution
   field :createClap, mutation: Mutations::CreateClap
   field :createTask, mutation: Mutations::CreateTask
-  
-  # TODO: Remove me
-  field :test_field, String, description: "An example field added by the generator", null: true
-
-  def test_field
-    "Hello World!"
-  end
 end
