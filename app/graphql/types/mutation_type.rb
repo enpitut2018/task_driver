c658d76ca242babe6f408505bbee0af1e9c93968
kunado <katsusand1@gmail.com>
2018-12-18 00:17:59 +0900

    add mutations about group model into mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -7,5 +7,9 @@ class Types::MutationType < GraphQL::Schema::Object
   field :updateTask, mutation: Mutations::UpdateTask
   field :deleteTask, mutation: Mutations::DeleteTask
 
+  field :createGroup, mutation: Mutations::CreateGroup
+  field :updateGroup, mutation: Mutations::UpdateGroup
+  field :deleteGroup, mutation: Mutations::DeleteGroup
+
   field :createClap, mutation: Mutations::CreateClap
 end
