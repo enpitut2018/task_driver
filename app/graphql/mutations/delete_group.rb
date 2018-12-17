ab635bad750e0184531c0dbacfeba38c8136f3f8
kunado <katsusand1@gmail.com>
2018-12-18 00:12:59 +0900

    add delete group mutation

---
+++ app/graphql/mutations/delete_group.rb
@@ -0,0 +1,19 @@
+class Mutations::DeleteGroup < GraphQL::Schema::Mutation
+  graphql_name 'DeleteGroupMutation'
+  null false
+
+  argument :group_id, ID, description: 'グループID', required: true
+
+  field :group, Types::GroupType, null: false
+  field :errors, [String], null: false
+
+  def resolve(group_id:)
+    group = Group.find(group_id)
+
+    if group.destroy
+      { group: group, errors: [] }
+    else
+      { group: group, errors: group.errors.full_messages }
+    end
+  end
+end
