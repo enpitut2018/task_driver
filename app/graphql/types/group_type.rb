8fcef73c6fe10b46c08a8466ebebe1da56545575
kunado <katsusand1@gmail.com>
2018-12-18 11:11:37 +0900

    add parent_group, ancestor_groups and ancestor_and_self_groups fields

--- app/graphql/types/group_type.rb
+++ app/graphql/types/group_type.rb
@@ -9,8 +9,34 @@ class Types::GroupType < Types::BaseObject
   field :importance, Integer, '重要度', null: true
   field :visible, Boolean, '公開/非公開', null: true
   field :tasks, [Types::TaskType], 'タスク', null: true
+  field :parent_group, Types::GroupType, '親グループ', null: true
+  field :ancestor_groups, [Types::GroupType], '先祖グループ', null: true
+  field :ancestor_and_self_groups, [Types::GroupType], '自分と先祖グループ', null: true
 
   def tasks
     object.tasks
+  end
+
+  def parent_group
+    Group.find(object.parent_id) if !object.parent_id.nil?
+  end
+
+  def ancestor_groups
+    tmp_parent = Group.find(parent_id) if !object.parent_id.nil?
+    ancestor_groups = []
+    while !tmp_parent.nil?
+      ancestor_groups.push(tmp_parent)
+      if !tmp_parent.parent_id.nil?
+        tmp_parent = Group.find(tmp_parent.parent_id)
+      else
+        tmp_parent = nil
+      end
+    end
+    ancestor_groups
+  end
+
+  def ancestor_and_self_groups
+    ancestor_and_self_groups = [Group.find(object.id)]
+    ancestor_and_self_groups.push(ancestor_groups).flatten!
   end
 end
