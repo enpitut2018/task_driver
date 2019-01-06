52b11e588609d8359072d89726f91c8ce71f3333
kunado <katsusand1@gmail.com>
2019-01-06 15:44:14 +0900

    add scope to get finished instances into contribution and task model

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -6,4 +6,6 @@ class Contribution < ApplicationRecord
   enum status: { active: true, inactive: false }
 
   validates :status, uniqueness: { scope: [:user_id, :task_id], if: 'active?' }
+
+  scope :finished, -> { where.not(finished_at: nil) }
 end
